a64654302a3e50645792ae310c345e19e794d764
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-16 04:23:18 -0400

    Added varDump function

--- header.php
+++ header.php
@@ -5,6 +5,15 @@ define("nl", "<br />");
 // include database connection
 require_once 'config/connection.php';
 
+// Dump list of variables passed in
+function varDump(...$Variables){
+
+    echo '<pre class=container>';
+    foreach($Variables as $var){
+        var_dump($var);
+    }
+    echo '</pre>';
+}
 
  // <!-- Latest compiled and minified CSS -->
 echo '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">';
