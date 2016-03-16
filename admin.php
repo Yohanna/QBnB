8882aaad109b3bb24e109844e13ccaf1ddf330bf
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-16 13:38:04 -0400

    Display All properties in QBnB along with buttons to make certain actions

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@
 // Admin page. Shows a list of all available properties in QBnB with CRUD functionality.
 
 require_once 'header.php';
+require_once 'navbar.php';
 
 //Create a user session or resume an existing one
 session_start();
@@ -29,10 +30,10 @@ if($stmt = $con->prepare($query)){
     if($num>0){
 
         //If the user_id matches a user in our db, get the is_admin value
-        $myrow = $result->fetch_assoc();
+        $row = $result->fetch_assoc();
 
         // If the current logged in user is NOT an admin, redirect to profile.php
-        if($myrow['is_admin'] == 0){
+        if($row['is_admin'] == 0){
             //Redirect the browser to the profile editing page and kill this page.
             header("Location: profile.php");
             die();
@@ -47,8 +48,77 @@ else {
 
 // Display all properties in the db
 
-echo "You're an admin" . nl;
+echo "<h1>Hello Admin</h1>" . nl;
+
 
 ?>
 
 
+<html>
+<body>
+
+    <?php
+    // Select all properties
+    $query = "SELECT address, district, type, price, FName, LName FROM properties, user where supplier_id = user_id";
+
+    // prepare query for execution
+    if($stmt = $con->prepare($query)){
+
+        // Execute the query
+        $stmt->execute();
+
+        // Get Results
+        $result = $stmt->get_result();
+
+        // Get the number of rows returned
+        $num = $result->num_rows;;
+
+        if($num == 0)
+          echo "Failed to prepare the SQL";
+        else {
+        ?>
+
+        <div class="container">
+        <h2>Current Properties in QBnB</h2>
+        <table class="table table-bordered table-striped" >
+        <thead>
+        <tr>
+          <th>Address</th>
+          <th>District</th>
+          <th>Type</th>
+          <th>Price per month</th>
+          <th>Supplier Name</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+          while($row = $result->fetch_assoc()):
+        ?>
+        <tr>
+          <td><?=$row['address'] ?></td>
+          <td><?=$row['district']?></td>
+          <td><?=$row['type']?></td>
+          <td><?=$row['price']?></td>
+          <td><?=$row['FName'] . ' ' . $row['LName']?></td>
+          <td>
+          <button type="button" class="btn btn-info">View</button>
+          <button type="button" class="btn btn-warning">Edit</button>
+          <button type="button" class="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+
+          <?php
+          endwhile; //for fetching $row
+        } // end else for '$num == 0'
+      } // end if $stmt prepare
+      ?>
+        </tbody>
+        </table>
+        </div>
+
+
+
+</body>
+</html>
