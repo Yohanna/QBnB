9b78eadc705add72550bde208a5dd1b576da840b
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-18 22:23:27 -0400

    Add My Properties, Add a property, Profile, Admin
    * Also added glyphicons for those pages.

--- navbar.php
+++ navbar.php
@@ -1,5 +1,7 @@
 <?php
-    require_once 'header.php';
+
+require_once 'header.php';
+
 ?>
 
 <html>
@@ -13,13 +15,24 @@
                         <a class="navbar-brand" href="search.php">QBnB</a>
                     </div>
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="search.php">Home</a></li>
-                        <li><a href="profile.php">Profile</a></li>
-                        <li><a href="#">Page 3</a></li>
+                        <li class="active"><a href="search.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">My Properties
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Add a property</a></li>
+                            </ul>
+                        </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+                    <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+                    <?php
+                    // Display Admin link in navbar if current logged in user is an admin
+                    if( isset( $_SESSION['admin']) && $_SESSION['admin'] == true ){
+                        echo '<li><a href="admin.php"><span class="glyphicon glyphicon-eye-open"></span> Admin</a></li>';
+                    }
+                    ?>
+                    <li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
                     </ul>
                 </div>
             </nav>
