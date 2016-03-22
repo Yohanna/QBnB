c1a043c28988dcbb529f31f45e9e0309b327bb80
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-22 00:50:53 -0400

    CSS files are already included in header.php

--- search.php
+++ search.php
@@ -1,26 +1,20 @@
 <?php
 
+require_once 'header.php';
 
 ?>
 
 <!DOCTYPE HTML>
 <html>
 <head>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-
-	<!-- Latest compiled JavaScript -->
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
+
 <body>
 	<div class="container">
 		<h3 class="text-center"> Please Fill Out A Search Form </h3>
 	</div>
 	<form class = "form-horizontal" role = "form">
-		<div class = "form-group"> 
+		<div class = "form-group">
 			<label class = "control-label col-sm-2" for = "type"> Property Type: </label>
 			<div class="col-sm-offset-2 col-sm-10">
 				<div class="radio">
@@ -38,21 +32,21 @@
 			</div>
 		</div>
 
-		<div class = "form-group"> 
+		<div class = "form-group">
 			<label class = "control-label col-sm-2" for = "type"> District: </label>
 			<div class="col-sm-offset-2 col-sm-10">
 				<select class="form-control" id="Distract">
-					
+
 				</select>
 			</div>
 		</div>
 
-		<div class = "form-group"> 
+		<div class = "form-group">
 			<label class = "control-label col-sm-2" for = "type"> Point Of Interest: </label>
-			
+
 		</div>
 
-		<div class = "form-group"> 
+		<div class = "form-group">
 			<label class = "control-label col-sm-2" for = "type"> Features: </label>
 			<div class="col-sm-offset-2 col-sm-10">
 				<div class="checkbox">
