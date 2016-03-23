e6247a5a1893a3254b98991b5cb7adfac096d075
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-23 17:11:52 -0400

    Don't display any tabs if no user is signed in

--- navbar.php
+++ navbar.php
@@ -23,31 +23,37 @@ if( userLoggedIn() ){
             <nav class="navbar navbar-inverse">
                 <div class="container-fluid">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="search.php">QBnB</a>
+                        <a class="navbar-brand">QBnB</a>
                     </div>
-                    <ul class="nav navbar-nav">
+
+                    <?php
+                    if(userLoggedIn()){
+                        echo
+                        '<ul class="nav navbar-nav">
                         <li class="active"><a href="search.php"><span class="glyphicon glyphicon-search"></span> Search</a></li>
                         <li><a href="user_properties.php"><span class="glyphicon glyphicon-home"></span> My Properties</a></li>
                         <li><a href="add_property.php"><span class="glyphicon glyphicon-plus"></span> Add a property</a></li>
                         <li><a href="#"><span class="glyphicon glyphicon-th-list"></span> My Bookings</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                    <?php
-                    // Display who's signed in
-                    if(userLoggedIn())
-                        echo '<li><p class="navbar-text">Signed in as '.$row['FName'] . ' ' . $row['LName'].'</p></li>';
-                    ?>
+                        </ul>';
 
+                        // Right Navbar section
+                        echo '<ul class="nav navbar-nav navbar-right">';
 
-                    <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-                    <?php
-                    // Display Admin link in navbar if current logged in user is an admin
-                    if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']){
-                        echo '<li><a href="admin.php"><span class="glyphicon glyphicon-eye-open"></span> Admin</a></li>';
+                        // Display who's signed in
+                        echo '<li><p class="navbar-text">Signed in as '.$row['FName'] . ' ' . $row['LName'].'</p></li>
+                            <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>';
+
+                        // Display Admin link in navbar if current logged in user is an admin
+                        if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']){
+                            echo '<li><a href="admin.php"><span class="glyphicon glyphicon-eye-open"></span> Admin</a></li>';
+                        }
+                    echo '<li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>';
+                    } // if(userLoggedIn())
+                    else{
+                        echo '<ul class="nav navbar-nav navbar-right">';
                     }
-                    if(userLoggedIn())
-                        echo '<li><a href="index.php?logout=1"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>';
                     ?>
+
                     <li><a href="about.php"><span class="glyphicon glyphicon-info-sign"></span> About</a></li>
                     </ul>
                 </div>
