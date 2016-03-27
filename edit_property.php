e5ea14750e87ab9e6ec8d4ac90c78ba280850340
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-27 00:12:10 -0400

    Added 'navbar'

--- edit_property.php
+++ edit_property.php
@@ -1,5 +1,4 @@
 <?php
-
-echo "Only an admin or a property ownser can edit a property";
+require_once 'navbar.php';
 
 ?>
--- view_property.php
+++ view_property.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'navbar.php';
+
 echo "View property details, pictures, reviews";
 
 
