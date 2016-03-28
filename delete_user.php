d36e811b6dcaed3ce37b4d9400aa6fdf4e5c5d9b
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-27 22:55:17 -0400

    Created 'delete_user.php', #30

---
+++ delete_user.php
@@ -0,0 +1,10 @@
+<?php
+
+// Display a confirmation that an admin wants to delete a user from  Qbnb.
+// This will also delete their properties automatically.
+
+
+require_once 'header.php';
+require_once 'navbar.php';
+
+?>
