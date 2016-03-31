3203573b7ce491bb3b1be90a64393c6e8cc609d6
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-03-30 22:16:12 -0400

    Some text formatting

--- about.php
+++ about.php
@@ -8,16 +8,7 @@ require_once 'navbar.php';
     </head>
 
     <body>
-        <h4>The Queen’s Alumnae BnB service (QBnB) will facilitate matching alumnae intending to
-travel to a city with other alumnae offering accommodations to rent in that city. Alumnae
-must first become members of QBnB. Members with properties to rent, who we can refer
-to as suppliers, will register these accommodations with the service. Other members
-planning to travel, who we can refer to as consumers, will use QBnB to search for
-accommodations in their destination city. When a consumer finds an accommodation they
-are interested in they can check the availability of the accommodation for their desired
-dates and then submit a request to the supplier to rent the accommodation for a particular
-booking period. If the supplier agrees to the request they submit a confirmation to QBnB
-that changes the availability of the accommodation, otherwise they reject the booking.</h4>
+        <h3 style="text-align: center;" class="col-md-10 col-md-offset-1">The Queen’s Alumnae BnB service (QBnB) will facilitate matching alumnae intending to travel to a city with other alumnae offering accommodations to rent in that city. Alumnae must first become members of QBnB. Members with properties to rent, who we can refer to as suppliers, will register these accommodations with the service. Other members planning to travel, who we can refer to as consumers, will use QBnB to search for accommodations in their destination city. When a consumer finds an accommodation they are interested in they can check the availability of the accommodation for their desired dates and then submit a request to the supplier to rent the accommodation for a particular booking period. If the supplier agrees to the request they submit a confirmation to QBnB that changes the availability of the accommodation, otherwise they reject the booking.</h3>
     </body>
 
 
